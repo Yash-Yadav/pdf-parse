7f74a8f600b42f29cb41dcac2e32ea4512fbd9ab
Yash-Yadav <yash-yadav@outlook.com>
2019-09-16 12:07:03 +0530

    Commit

--- parse.js
+++ parse.js
@@ -39,28 +39,3 @@ new pdfreader.PdfReader().parseFileItems("table.pdf", function(err, item) {
     table.processItem(item, columnQuantitizer(item));
   }
 });
-
-// var pdfreader = require("pdfreader");
- 
-// var rows = {}; // indexed by y-position
- 
-// function printRows() {
-//   Object.keys(rows) // => array of y-positions (type: float)
-//     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
-//     .forEach(y => console.log((rows[y] || []).join("")));
-// }
- 
-// new pdfreader.PdfReader().parseFileItems("CV_ErhanYasar.pdf", function(
-//   err,
-//   item
-// ) {
-//   if (!item || item.page) {
-//     // end of file, or page
-//     printRows();
-//     // console.log("PAGE:", item.page);
-//     rows = {}; // clear rows for next page
-//   } else if (item.text) {
-//     // accumulate text items into rows object, per line
-//     (rows[item.y] = rows[item.y] || []).push(item.text);
-//   }
-// });
